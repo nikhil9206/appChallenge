3e1266fe3199254af23ca066fa8b133d6114bffb
Roko87 <rohan.konda@hotmail.com>
2023-09-10 17:34:17 -0500

    lib.js upload

---
+++ lib.js
@@ -0,0 +1,51 @@
+//Changes the text of an element
+function setText(element, text) {
+    document.getElementById(element).textContent = text;
+}
+
+//Checks if a element is a certain string
+function isText(element, text) {
+    if (document.getElementById(element).textContent == text) {
+        return true; 
+    } else {
+        return false;
+    }
+}
+
+//Changes the number of an element by a certain number
+function changeValueBy(element, number) {
+    if (document.getElementById(element).textContent != NaN) {
+        var res = 0;
+        res = document.getElementById(element).textContent;
+        console.log(res);
+        res = res + number;
+        console.log(res + ", " + number);
+        document.getElementById(element).textContent = res;
+    } else {
+        return NaN;
+    }
+}
+
+//Changes the number of an element by a certain number
+function getNum(element) {
+    if (document.getElementById(element).textContent != NaN) {
+        var number =  document.getElementById(element).textContent;
+        return number;
+     }
+}
+
+function getText(element) {
+        var text = document.getElementById(element).value;
+        return text;   
+}
+
+function isNum(element) {
+    var bool = true;
+    if (document.getElementById(element).textContent != Nan) {
+        bool = true;
+    }
+    else {
+        bool = false;
+    }
+    return bool;
+}
