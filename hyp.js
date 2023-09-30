1cfbf11e8fc271ab8f4a1c17515333ef2192ca4e
Roko87 <rohan.konda@hotmail.com>
2023-09-30 11:51:27 -0500

    930-1

--- hyp.js
+++ hyp.js
@@ -1,3 +1,38 @@
+function hyprisk() {
+    var sys = parseInt(getText("sysBP_input"));
+    var dia = parseInt(getText("diaBP_input"));
+
+    if (isNaN(sys) || isNaN(dia)) {
+        setText("alerttext", "Please enter a valid number for the fields");
+    } else if (sys < 90 || sys > 300) {
+        setText("alerttext", "Systolic pressure must be between 90 and 300");
+    } else if (dia < 50 || dia > 240) {
+        setText("alerttext", "Diastolic pressure must be between 50 and 240");
+    } else if (dia > sys) {
+        setText("alerttext", "Diastolic pressure must be lower than systolic pressure"); 
+    } else if (sys < 120 && dia < 80) {
+        setText("alerttext", "");
+        setText("classtext", "Low");
+        setText("desctext", "Your blood pressure is at a healthy level.")
+    } else if (sys < 130 && dia < 80) {
+        setText("alerttext", "");
+        setText("classtext", "Elevated");
+        setText("desctext", "You're in a prehypertensive state. You should watch your blood pressure levels and try to maintain a healthy lifestyle.")
+    } else if (sys < 140 || dia < 90) {
+        setText("alerttext", "");
+        setText("classtext", "Stage 1 Hypertension");
+        setText("desctext", "You have hypertension. You should consult a doctor and try to maintain a healthy lifestyle.")
+    } else if (sys < 180 || dia < 120) {
+        setText("alerttext", "");
+        setText("classtext", "Stage 2 Hypertension");
+        setText("desctext", "You have hypertension. You should consult a doctor and try to maintain a healthy lifestyle.")
+    } else {
+        setText("alerttext", "");
+        setText("classtext", "Hypertensive Crisis");
+        setText("desctext", "You have hypertension. You should consult a doctor and try to maintain a healthy lifestyle.")
+    }
+}
+
 function risk() {
     var sys = getText("sys_input");
     var dia = getText("dia_input")
