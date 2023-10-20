42961de28bde3ba9891b14f1c07b5aba7bc3b788
Nikhil <109251761+nikhil9206@users.noreply.github.com>
2023-10-19 19:46:41 -0500

    Update vax.js

--- vax.js
+++ vax.js
@@ -1,5 +1,5 @@
 function risk() {
-    var age = getText("age_input");
+    var age = parseInt(getText("age_input"));
     var flu = false;
     var covid = false;
     var tdp = false;
@@ -9,6 +9,19 @@ function risk() {
     var zoster = false;
     var rsv = false;
     var pneumonia = false;
+    var hepatitis = false;
+
+
+    var fluBlock = document.getElementById("flu");
+    var covidBlock = document.getElementById("covid");
+    var tdpBlock = document.getElementById("tdp");
+    var mmrBlock = document.getElementById("mmr");
+    var hpvBlock = document.getElementById("hpv");
+    var meningitisBlock = document.getElementById("meningitis");
+    var zosterBlock = document.getElementById("zoster");
+    var rsvBlock = document.getElementById("rsv");
+    var pneumoniaBlock = document.getElementById("pneumonia");
+    var hepatitisBlock = document.getElementById("hepatitis");
 
 
     if (isNaN(age)) {
@@ -24,64 +37,105 @@ function risk() {
         covid = true;
         tdp = true;
         mmr = true;
-        hpv = age <= 25;
-        meningitis = age <= 30;
-        zoster = age >= 50;
-        rsv = age >= 60;
-        pneumonia = age >= 65;
+        if (age <= 25) {
+            hpv = true;
+        }
+        if (age <= 30) {
+            meningitis = true;
+        }
+        if (age >= 50) {
+            zoster = true;
+        }
+        if (age >= 60) {
+            rsv = true;
+        }
+        if (age >= 65) {
+            pneumonia = true;
+        }
+        if (age <= 60) {
+            hepatitis = true;
+        }
     } 
 
 
     if (flu) {
+        fluBlock.style.display = "block";
         setText("flutext", "Influenza (yearly)");
     } else {
+        fluBlock.style.display = "none";
         setText("flutext", "");
     }
     if (covid) {
-        setText("covidtext", "COVID-19 (as recommended by CDC)")
+        covidBlock.style.display = "block";
+        setText("covidtext", "COVID-19 (as recommended by CDC)");
     } else {
+        covidBlock.style.display = "none";
         setText("covidtext", "");
     }
 
     if (tdp) {
-        setText("tdptext", "Tetanus, diphtheria, and pertussis (every 10 years)")
+        tdpBlock.style.display = "block";
+        setText("tdptext", "Tetanus, diphtheria, and pertussis (every 10 years)");
     } else {
+        tdpBlock.style.display = "none";
         setText("tdptext", "");
     }
 
     if (mmr) {
-        setText("mmrtext", "MMR (at least one as an adult)")
+        mmrBlock.style.display = "block";
+        setText("mmrtext", "MMR (at least one as an adult)");
     } else {
+        mmrBlock.style.display = "none";
         setText("mmrtext", "");
     }
 
-    if (hpv) {
-        setText("hpvtext", "HPV (at least once)")
-    } else {
-        setText("hpvtext", "");
-    }
+   
 
     if (meningitis) {
-        setText("meningitistext", "Meningitis (at least once)")
+        meningitisBlock.style.display = "block";
+        setText("meningitistext", "Meningitis (at least once)");
     } else {
+        meningitisBlock.style.display = "none";
         setText("meningitistext", "");
     }
 
     if (zoster) {
-        setText("zostertext", "Zoster (at least once)")
+        zosterBlock.style.display = "block";
+        setText("zostertext", "Zoster (at least once)");
     } else {
+        zosterBlock.style.display = "none";
         setText("zostertext", "");
     }
 
     if (rsv) {
-        setText("rsvtext", "RSV (at least once)")
+        rsvBlock.style.display = "block";
+        setText("rsvtext", "RSV (at least once)");
     } else {
+        rsvBlock.style.display = "none";
         setText("rsvtext", "");
     }
 
     if (pneumonia) {
-        setText("pneumoniatext", "Pneumonia (at least once)")
+        pneumoniaBlock.style.display = "block";
+        setText("pneumoniatext", "Pneumonia (at least once)");
     } else {
+        pneumoniaBlock.style.display = "none";
         setText("pneumoniatext", "");
+    }
+
+    if (hepatitis) {
+        hepatitisBlock.style.display = "block";
+        setText("hepatitistext", "Hepatitis (Should recive 2-4 doses)");
+    } else {
+        hepatitisBlock.style.display = "none";
+        setText("hepatitistext", "");
+    }
+
+    if (hpv) {
+        hpvBlock.style.display = "block";
+        setText("hpvtext", "HPV (at least once)");
+    } else {
+        hpvBlock.style.display = "none";
+        setText("hpvtext", "");
     }
 }
